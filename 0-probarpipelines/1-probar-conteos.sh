5f8f81c855ca2564ec9fd942adbe895b8e67016a
Israel Aguilar <iaguilaror@gmail.com>
2024-07-05 02:16:35 -0600

    set limits for pipeline testing

--- 0-probarpipelines/1-probar-conteos.sh
+++ 0-probarpipelines/1-probar-conteos.sh
@@ -2,5 +2,5 @@
 nextflow run nf-core/rnaseq \
 	-profile test,docker \
 	--outdir prueba-conteos \
-	--max_cpus 2 --max_memory 4.GB
+	--max_cpus 2 --max_memory 3.GB
 
--- 0-probarpipelines/2-probar-DGE.sh
+++ 0-probarpipelines/2-probar-DGE.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-nextflow run nf-core/differentialabundance -profile test,docker --outdir prueba-DGE
+nextflow run nf-core/differentialabundance \
+	-profile test,docker \
+	--outdir prueba-DGE \
+	--max_cpus 2 --max_memory 3.GB
